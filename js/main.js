48f4b2588d3018ababe1b855b6a3115bdc3ac839
trachanh1609 <Vu@Users-MacBook-Pro.local>
2017-11-25 21:04:59 +0200

    add more buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+// $(function() {
   const model = {
     init () {
 
@@ -10,15 +10,43 @@ $(function() {
     init () {
       model.init();
       view.init();
+    },
+    nextToTaskView() {
+      view.goToTaskView();
+    },
+    backToGroupView() {
+      view.goToGroupView();
     }
   };
 
   const view = {
     init () {
+      this.groupView = $('#groupView');
+      this.taskView = $('#taskView');
+      this.cards = $('.card');
 
+      this.taskView.hide();
+      this.addEventToCards();
+    },
+    goToTaskView(){
+      this.groupView.slideUp(400);
+      this.taskView.slideDown(400);
+    },
+    goToGroupView(){
+      this.taskView.slideUp(400);
+      this.groupView.slideDown(400);
+    },
+    addEventToCards(){
+      let self = this
+      self.cards.each(function(){
+        $(this).click(function(){
+          self.goToTaskView();
+        })
+      });
     }
+
   };
 
   controller.init();
 
-});
+// });
