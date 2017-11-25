2ce6799054132c7d9eda4a3aa6af89b0d9e2ff73
trachanh1609 <Vu@Users-MacBook-Pro.local>
2017-11-25 16:53:17 +0200

    clean the project for taskit

--- js/main.js
+++ js/main.js
@@ -1,73 +1,8 @@
-var data = {
-  'lines': [
-    {
-      'id': 1,
-      'origin': {
-        'full': 'Sí, sabes que ya llevo un rato mirándote',
-        'wordByWord':{
-          'one': 'Sí',
-          'two': 'sabes',
-          'three': 'que'
-        }
-      },
-      'english': {
-        'full': 'Yes, you know that I have been watching you for a while',
-        'wordByWord':{
-          'one': 'Yes',
-          'two': 'You',
-          'three': 'know'
-        }
-      }
-    },
-    {
-      'id': 2,
-      'origin': {
-        'full': 'Tengo que bailar contigo hoy ',
-        'wordByWord':{
-          'one': 'Tengo',
-          'two': 'que',
-          'three': 'bailar'
-        }
-      },
-      'english': {
-        'full': 'I have to dance with you today',
-        'wordByWord':{
-          'one': 'I',
-          'two': 'have to',
-          'three': 'dance'
-        }
-      }
-    },
-    {
-      'id': 3,
-      'origin': {
-        'full': 'Vi que tu mirada ya estaba llamándome ',
-        'wordByWord':{
-          'one': 'Vi',
-          'two': 'que',
-          'three': 'tu'
-        }
-      },
-      'english': {
-        'full': 'I saw that your look was already calling me',
-        'wordByWord':{
-          'one': 'I',
-          'two': 'saw',
-          'three': 'that'
-        }
-      }
-    }
-  ]
-
-}
-
 $(function() {
   const model = {
     init () {
-      this.data = data ;
 
-      // console.log(this.lyrics.three.origin.full);
-      // console.log('model initiated');
+
     }
   };
 
@@ -75,36 +10,12 @@ $(function() {
     init () {
       model.init();
       view.init();
-      // console.log('controller initiated');
-      view.updateOrigin(model.data);
-      view.updateTranslated(model.data);
     }
   };
 
   const view = {
     init () {
-      this.origin = $('#origin-lyrics');
-      this.translated = $('#translated-lyrics');
-      // console.log('view initiated');
 
-    },
-    updateOrigin(lyrics){
-      let self = this;
-      self.origin.html('');
-      lyrics.lines.forEach(function(line){
-        let lineContent = $('<p>' +line.origin.full  + '</p>')
-        // console.log(line.origin.full);
-        self.origin.append(lineContent);
-      });
-    },
-    updateTranslated(lyrics){
-      let self = this;
-      self.translated.html('');
-      lyrics.lines.forEach(function(line){
-        let lineContent = $('<p>' +line.english.full  + '</p>')
-        // console.log(line.origin.full);
-        self.translated.append(lineContent);
-      });
     }
   };
 
